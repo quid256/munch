c2e63f281da87a29dace201d2b1e98d9c24b99bf
Chris Winkler <quidnovum@gmail.com>
2021-10-09 03:07:43 -0400

    Add compression + encryption for generated HTML
    - Compression is useful because it decreases the size of the rendered
      HTML
    - Encryption is useful because I don't want everyone to have access to
      my personal library of recipes. Some of them may have been lifted from
      proprietary sources, and making them available to the public seems
      unwise
    
    These are both in service of the ultimate goal of putting one of these
    rendered recipe books on some publicly hosted place so it's easy to
    access from anywhere via phone.

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
 	"log"
 	"os"
 	"path"
@@ -23,13 +30,46 @@ func check(err error) {
 
 // renderRecipes takes a list of recipe objects, renders them into the HTML
 // template file, and writes the result to the appropriate output file name.
-func renderRecipes(recipes []recipe, template *raymond.Template, outputFile string) error {
-	// Substitute the recipe data into the HTML template file
+func renderRecipes(recipes []recipe, template *raymond.Template, outputFile string, password string) error {
+	// Marshall of the recipes into JSON
+	recipeJSON, err := json.Marshal(recipes)
+	if err != nil {
+		return err
+	}
+
+	// Compress the JSON using gzip
+	var buf bytes.Buffer
+	zw := gzip.NewWriter(&buf)
+	_, err = zw.Write(recipeJSON)
+	check(err)
+	err = zw.Close()
+	check(err)
+
+	b := buf.Bytes()
+
+	// Use the provided password to encrypt the compressed JSON
+	hashedPassword := sha256.Sum256([]byte(password))
+	iv, err := base64.StdEncoding.DecodeString("AAAAAAAAAAAAAAAAAAAAAA==")
+	check(err)
+
+	// (some random padding necessary for AES, lifted from some random Github gist)
+	n := aes.BlockSize - (len(b) % aes.BlockSize)
+	pb := make([]byte, len(b)+n)
+	copy(pb, b)
+	copy(pb[len(b):], bytes.Repeat([]byte{byte(n)}, n))
+
+	c, err := aes.NewCipher(hashedPassword[:])
+	cfbdec := cipher.NewCBCEncrypter(c, iv)
+	ciphertext := make([]byte, len(pb))
+	cfbdec.CryptBlocks(ciphertext, pb)
+
+	// Insert the encrypted recipe data into the appropriate spot in the
+	// generated HTML
 	htmlRes := template.MustExec(map[string]interface{}{
-		"recipes": recipes,
+		"recipeData": base64.RawStdEncoding.EncodeToString(ciphertext),
 	})
 
-	// Create / open the output file for writing
+	// Write the generated HTML to a file
 	f, err := os.Create(outputFile)
 	if err != nil {
 		return err
@@ -89,25 +129,33 @@ func readRecipeDir(recipeFolder string) ([]recipe, error) {
 
 func main() {
 	app := &cli.App{
-		Flags: []cli.Flag{
-			&cli.StringFlag{
-				Name:    "output",
-				Aliases: []string{"o"},
-				Usage:   "Render recipe book to the specified file",
-				Value:   "render.html",
-			},
-			&cli.StringFlag{
-				Name:    "template",
-				Aliases: []string{"t"},
-				Usage:   "File to use for recipe book template",
-				Value:   "template.html",
-			},
-		},
+		Name:  "munch",
+		Usage: "Render you a cookbook",
 		Commands: []*cli.Command{
 			{
 				Name:    "render",
 				Aliases: []string{"r"},
 				Usage:   "Render the specified directory into a dynamic cookbook HTML",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:    "output",
+						Aliases: []string{"o"},
+						Usage:   "Render recipe book to the specified file",
+						Value:   "render.html",
+					},
+					&cli.StringFlag{
+						Name:    "template",
+						Aliases: []string{"t"},
+						Usage:   "File to use for recipe book template",
+						Value:   "template.html",
+					},
+					&cli.StringFlag{
+						Name:    "pass",
+						Aliases: []string{"p"},
+						Usage:   "Password to use for encryption (default is 'password')",
+						Value:   "password",
+					},
+				},
 				Action: func(c *cli.Context) error {
 					if c.Args().Len() != 1 {
 						panic("Only 1 argument allowed")
@@ -126,7 +174,7 @@ func main() {
 					recipes, err := readRecipeDir(fp)
 					check(err)
 
-					check(renderRecipes(recipes, template, c.String("output")))
+					check(renderRecipes(recipes, template, c.String("output"), c.String("pass")))
 					writeNutritionCache()
 
 					return nil
@@ -136,6 +184,26 @@ func main() {
 				Name:    "watch",
 				Aliases: []string{"w"},
 				Usage:   "Watch the specified directory for recipe changes and update the cookbook HTML",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:    "output",
+						Aliases: []string{"o"},
+						Usage:   "Render recipe book to the specified file",
+						Value:   "render.html",
+					},
+					&cli.StringFlag{
+						Name:    "template",
+						Aliases: []string{"t"},
+						Usage:   "File to use for recipe book template",
+						Value:   "template.html",
+					},
+					&cli.StringFlag{
+						Name:    "pass",
+						Aliases: []string{"p"},
+						Usage:   "Password to use for encryption (default is 'password')",
+						Value:   "password",
+					},
+				},
 				Action: func(c *cli.Context) error {
 					if c.Args().Len() != 1 {
 						panic("Only 1 argument allowed")
@@ -160,7 +228,7 @@ func main() {
 
 					renderBook := func() {
 						log.Println("Rendering book...")
-						check(renderRecipes(recipes, template, c.String("output")))
+						check(renderRecipes(recipes, template, c.String("output"), c.String("pass")))
 						writeNutritionCache()
 					}
 
